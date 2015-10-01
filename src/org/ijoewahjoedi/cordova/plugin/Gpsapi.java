c691bfb52faf9e3c96a54d9e5ae7ff2b1e685735
ijoewahjoedi <ijoe.wahjoedi@gmail.com>
2015-10-01 10:51:57 +0700

    Add Gpsapi.java

---
+++ src/org/ijoewahjoedi/cordova/plugin/Gpsapi.java
@@ -0,0 +1,35 @@
+package org.ijoewahjoedi.cordova.plugin;
+
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class is an interface for Android GPS sensor
+ */
+
+public class Gpsapi extends CordovaPlugin {
+  @Override
+  public boolean execute (String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("echo")) {
+      String message = args.getString(0);
+      this.echo(message callbackContext);
+      return true;
+    }
+    return false;
+  }
+
+  private void echo (String message, CallbackContext callbackContext) {
+    if (message != null && message.length() > 0) {
+      callbackContext.success(message);
+    } else {
+      callbackContext.error("Expected one non-empty string argument.");
+    }
+  }
+
+
+}
+
+
