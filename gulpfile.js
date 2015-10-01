d41b3e6f5908fdbbfdce4b09afab9c115ea25906
ijoewahjoedi <ijoe.wahjoedi@gmail.com>
2015-10-01 15:38:48 +0700

    default

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/* File: gulpfile.js */
+
+// grab our packages
+var gulp   = require('gulp')
+
+// define the default task and add the watch task to it
+gulp.task('default', ['build']);
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.src('www/*.js').pipe(gulp.dest('dest'));
+  });
