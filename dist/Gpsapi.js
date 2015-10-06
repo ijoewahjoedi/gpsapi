45318240a1d5427cbc5a671bca4c3908359132a9
ijoewahjoedi <ijoe.wahjoedi@gmail.com>
2015-10-06 11:34:24 +0700

    default

--- dist/Gpsapi.js
+++ dist/Gpsapi.js
@@ -2,17 +2,32 @@ var exec = require('cordova/exec');
 function Gpsapi() { 
   console.log("Gpsapi.js: is created");
 }
-Gpsapi.prototype.echo = function(aString){
-  console.log("Gpsapi.js: echo");
-  exec(
-      function(result){ 
-        /*alert("OK" + reply);*/ 
-      }, 
-      function(result){ 
-        /*alert("Error" + reply);*/ 
-      },
-      "Gpsapi",aString,[]
-      );
-} 
-var gpsapi = new Gpsapi(); 
-module.exports = gpsapi;
+// Gpsapi.prototype.getLocation = function(aString){
+//   console.log("Gpsapi.js: getLocation");
+//   exec(
+//       function(result){ 
+//         /*alert("OK" + reply);*/ 
+//       }, 
+//       function(result){ 
+//         /*alert("Error" + reply);*/ 
+//       },
+//       "Gpsapi",aString,[]
+//       );
+// } 
+// var gpsapi = new Gpsapi(); 
+// module.exports = gpsapi;
+//
+
+
+// cordova.define('cordova/plugin/Gpsapi', function(require, export, module){
+  var exec = require('cordova/exec');
+
+  var Gpsapi = function() {};
+
+  Gpsapi.prototype.getLocation = function(onSuccess, onFailed){
+    exec(onSuccess, onFailed, 'Gpsapi', 'getLocation', []);
+  }
+
+  module.exports = new Gpsapi();
+
+// });
